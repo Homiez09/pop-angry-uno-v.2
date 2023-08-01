f53331f3fdec2f4f0408a810a1a8fac5c2ef987c
Phumrapee Soenvanichakul <phumsoen123@gmail.com>
2023-08-01 13:59:39 +0700

    Update

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const port = 5000
-
-app.set("view engine", "ejs");
+const { index } = require('./views/index.js');
+//app.set("view engine", "ejs");
 app.use('/static', express.static('public'))
 
 app.get('/', (req, res) => {
     //res.render("index");
-    res.send("helloq");
+    res.send(index);
 })
 
 app.listen(port, () => {
